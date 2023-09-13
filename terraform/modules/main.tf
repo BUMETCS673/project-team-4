aec03bd0feda7345de3e06d62a769c2b11c40b16
David Mulvihill <davidmulvihill@outlook.com>
2023-09-12 21:49:07 -0500

    adding subnets

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -2,9 +2,39 @@
 ## VPC Resources ##
 ###################
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "flask_vpc" {
   cidr_block       = "10.0.0.0/16"
   
+  tags = {
+    Name = "flask-app-vpc"
+  }
+}
+
+resource "aws_subnet" "flask_subnet_1a" {
+  availability_zone = "us-east-1a"  
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.flask_vpc.id
+
+  tags = {
+    Name = "flask-app-vpc"
+  }
+}
+
+resource "aws_subnet" "flask_subnet_1b" {
+  availability_zone = "us-east-1b"  
+  cidr_block = "10.0.2.0/24"
+  vpc_id     = aws_vpc.flask_vpc.id
+
+  tags = {
+    Name = "flask-app-vpc"
+  }
+}
+
+resource "aws_subnet" "flask_subnet_1c" {
+  availability_zone = "us-east-1c"  
+  cidr_block = "10.0.3.0/24"
+  vpc_id     = aws_vpc.flask_vpc.id
+
   tags = {
     Name = "flask-app-vpc"
   }
