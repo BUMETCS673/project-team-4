01865ba13bda5338cffe55e069ac1b51399b70cf
David Mulvihill <davidmulvihill@outlook.com>
2023-09-13 00:50:20 -0500

    adding ECS cluster, td, and data route table

--- terraform/modules/data.tf
+++ terraform/modules/data.tf
@@ -8,10 +8,3 @@ data "aws_region" "current" {}
 ## VPC Data ##
 ##################
 
-data "aws_route_table" "selected" {
-  vpc_id = aws_vpc.flask_vpc.id
-
-  tags = {
-    Name = "flask-app-rt"
-  }
-}
