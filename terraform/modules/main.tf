a5588a7f1c2e223f2243d397104acfc9fcb58f46
David Mulvihill <davidmulvihill@outlook.com>
2023-09-12 22:01:22 -0500

    adding s3 backend for tf state

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -3,39 +3,39 @@
 ###################
 
 resource "aws_vpc" "flask_vpc" {
-  cidr_block       = "10.0.0.0/16"
-  
+  cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "flask-app-vpc"
   }
 }
 
 resource "aws_subnet" "flask_subnet_1a" {
-  availability_zone = "us-east-1a"  
-  cidr_block = "10.0.1.0/24"
-  vpc_id     = aws_vpc.flask_vpc.id
+  availability_zone = "us-east-1a"
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = aws_vpc.flask_vpc.id
 
   tags = {
-    Name = "flask-app-vpc"
+    Name = "flask-app-subnet-1a"
   }
 }
 
 resource "aws_subnet" "flask_subnet_1b" {
-  availability_zone = "us-east-1b"  
-  cidr_block = "10.0.2.0/24"
-  vpc_id     = aws_vpc.flask_vpc.id
+  availability_zone = "us-east-1b"
+  cidr_block        = "10.0.2.0/24"
+  vpc_id            = aws_vpc.flask_vpc.id
 
   tags = {
-    Name = "flask-app-vpc"
+    Name = "flask-app-subnet-1b"
   }
 }
 
 resource "aws_subnet" "flask_subnet_1c" {
-  availability_zone = "us-east-1c"  
-  cidr_block = "10.0.3.0/24"
-  vpc_id     = aws_vpc.flask_vpc.id
+  availability_zone = "us-east-1c"
+  cidr_block        = "10.0.3.0/24"
+  vpc_id            = aws_vpc.flask_vpc.id
 
   tags = {
-    Name = "flask-app-vpc"
+    Name = "flask-app-subnet-1c"
   }
 }
