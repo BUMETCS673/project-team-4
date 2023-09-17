17c65361599d93583f9b25e348d0ffd88505e4d9
David Mulvihill <davidmulvihill@outlook.com>
2023-09-16 19:27:32 -0500

    adding task definition

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -69,12 +69,7 @@ resource "aws_ecs_task_definition" "flask_app_container" {
         }
       ]
     }
-  ])  
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b, us-east-1c]"
-  }
+  ])
 }
 
 ###################
