1102421e23f11e0d3ad8402794639471cb57997c
David Mulvihill <davidmulvihill@outlook.com>
2023-09-20 23:18:31 -0500

    creating rds

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -123,19 +123,25 @@ resource "aws_ecs_service" "flask_app_svc" {
 ########################
 
 resource "aws_rds_cluster" "example" {
-  
   cluster_identifier = "example"
-  engine             = "aurora-mysql"
-  engine_mode        = "serverless"
-  engine_version     = "8.0"
+  engine             = "aurora-postgresql"
+  engine_mode        = "provisioned"
+  engine_version     = "13.6"
   database_name      = "test"
   master_username    = "test"
   master_password    = "must_be_eight_characters"
 
-  scaling_configuration {
-    max_capacity = 256
-    min_capacity = 4
+  serverlessv2_scaling_configuration {
+    max_capacity = 1.0
+    min_capacity = 0.5
   }
+}
+
+resource "aws_rds_cluster_instance" "example" {
+  cluster_identifier = aws_rds_cluster.example.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.example.engine
+  engine_version     = aws_rds_cluster.example.engine_version
 }
 
 ###############################
