7cbd081956a182edbb9c1c593874855ed4d9fcae
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2023-09-23 10:06:41 -0700

    Super basic routing structure

--- app/src/app.py
+++ app/src/app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def login():
+def main():
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
+    
+    return render_template('landing_page.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    return render_template('landing_page.html')
