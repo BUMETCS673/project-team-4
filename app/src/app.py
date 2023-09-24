1ed783b4bfa2b5ae084d7e8659188c97df4942e0
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2023-09-23 18:04:09 -0700

    Found a temp API for testing

--- app/src/app.py
+++ app/src/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
-from themoviedb import aioTMDb
+from themoviedb import TMDb
+from justwatch import JustWatch
+from canistreamit import search, streaming, rental, purchase, dvd, xfinity
 
 app = Flask(__name__)
 
 
-#tmdb = aioTMDb(api_key='API Key when we recieve one')
+#tmdb = TMDb(api_key='API Key when we recieve one')
+
+showFinder= JustWatch(country='US')
 
 @app.route('/')
 def main():
@@ -20,12 +24,29 @@ def accessPage():
         email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
-    display_movies_and_tv_shows()
-    return render_template("landing_page.html")
+    print("Loading Movie Data")
+    popular_tv_shows = showFinder.search_for_item(content_types = ['show'])['items']
+    '''
+    for show in popular_tv_shows:
+        print(search(show['title']))
+        print(streaming(search(show['title'])[0]['_id']))
+    '''
+    popular_movies = showFinder.search_for_item(content_types = ['movie'])['items']
+    print(popular_tv_shows['items'][0]['title'])
+    return render_template('landing_page.html',
+                           popular_movies = popular_movies['items'],
+                           popular_tv_shows = popular_tv_shows['items'])
+    #display_movies_and_tv_shows()
+    #return render_template("landing_page.html")
 
 
-async def display_movies_and_tv_shows():
+def display_movies_and_tv_shows():
     print("Load and get Movie Data")
+    popular_tv_shows = showFinder.popular_tv_shows(limit=10)
+    popular_movies = showFinder.popular_movies(limit=10)
+    return render_template('landing_page.html',
+                           popular_movies=popular_movies,
+                           popular_tv_shows=popular_tv_shows)
     '''
     # Fetch a list of popular movies and TV shows (customize as needed)
     popular_movies = tmdb.movie.popular()
