2ff98b9b56899af0dbbc0104a6e727ed5da96ef0
David Mulvihill <davidmulvihill@outlook.com>
2023-09-26 21:16:57 -0500

    adding cert to listener

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -78,7 +78,7 @@ resource "aws_lb_listener" "flask_app_alb_443" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  certificate_arn   = aws_acm_certificate.domain_cert.arn
 
   default_action {
     type             = "forward"
